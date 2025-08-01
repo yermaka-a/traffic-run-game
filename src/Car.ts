ca76155cd1b8f64604b195ae9a2f534134812fc3
yermaka <ermak.080@mail.ru>
2025-08-01 17:52:05 +0800

    add windows for car

--- src/Car.ts
+++ src/Car.ts
@@ -1,34 +1,91 @@
 import * as THREE from "three";
+
 const vehicleColors = [0xa52523, 0xbdb638, 0x78b14b];
+
+const getCarFrontTexture = () => {
+  const canvas = document.createElement("canvas");
+  canvas.width = 64;
+  canvas.height = 32;
+  const context = canvas.getContext("2d");
+  if (context) {
+    context.fillStyle = "#ffffff";
+    context.fillRect(0, 0, 64, 32);
+    context.fillStyle = "#666666";
+    context.fillRect(8, 8, 48, 24);
+  }
+
+  return new THREE.CanvasTexture(canvas);
+};
+
+const getCarSideTexture = () => {
+  const canvas = document.createElement("canvas");
+  canvas.width = 128;
+  canvas.height = 32;
+  const context = canvas.getContext("2d");
+  if (context) {
+    context.fillStyle = "#ffffff";
+    context.fillRect(0, 0, 128, 32);
+    context.fillStyle = "#666666";
+    context.fillRect(10, 8, 38, 24);
+    context.fillRect(58, 8, 60, 24);
+  }
+
+  return new THREE.CanvasTexture(canvas);
+};
+
+const Wheel = () => {
+  const wheel = new THREE.Mesh(
+    new THREE.BoxGeometry(12, 33, 12),
+    new THREE.MeshLambertMaterial({ color: 0x333333 })
+  );
+  wheel.position.z = 6;
+  return wheel;
+};
+
+const pickRandom = (colors: number[]) => {
+  return colors[Math.floor(Math.random() * colors.length)];
+};
 
 export const Car = () => {
   const car = new THREE.Group();
 
-  const backWheel = new THREE.Mesh(
-    new THREE.BoxGeometry(12, 33, 12),
-    new THREE.MeshLambertMaterial({ color: 0x333333 })
-  );
-  backWheel.position.z = 6;
+  const backWheel = Wheel();
   backWheel.position.x = -18;
   car.add(backWheel);
 
-  const frontWheel = new THREE.Mesh(
-    new THREE.BoxGeometry(12, 33, 12),
-    new THREE.MeshLambertMaterial({ color: 0x333333 })
-  );
-  frontWheel.position.z = 6;
+  const frontWheel = Wheel();
   frontWheel.position.x = 18;
   car.add(frontWheel);
   const main = new THREE.Mesh(
     new THREE.BoxGeometry(60, 30, 15),
-    new THREE.MeshLambertMaterial({ color: 0xa52523 })
+    new THREE.MeshLambertMaterial({ color: pickRandom(vehicleColors) })
   );
   main.position.z = 12;
   car.add(main);
-  const cabin = new THREE.Mesh(
-    new THREE.BoxGeometry(33, 24, 12),
-    new THREE.MeshLambertMaterial({ color: 0xffffff })
-  );
+
+  const carFrontTexture = getCarFrontTexture();
+  carFrontTexture.center = new THREE.Vector2(0.5, 0.5);
+  carFrontTexture.rotation = Math.PI / 2;
+
+  const carBackTexture = getCarFrontTexture();
+  carBackTexture.center = new THREE.Vector2(0.5, 0.5);
+  carBackTexture.rotation = -Math.PI / 2;
+
+  const carRightSideTexture = getCarSideTexture();
+
+  const carLeftSideTexture = getCarSideTexture();
+
+  carLeftSideTexture.flipY = false;
+
+  const cabin = new THREE.Mesh(new THREE.BoxGeometry(33, 24, 12), [
+    new THREE.MeshLambertMaterial({ map: carFrontTexture }),
+    new THREE.MeshLambertMaterial({ map: carBackTexture }),
+    new THREE.MeshLambertMaterial({ map: carLeftSideTexture }),
+    new THREE.MeshLambertMaterial({ map: carRightSideTexture }),
+    new THREE.MeshLambertMaterial({ color: 0xffffff }), // top
+    new THREE.MeshLambertMaterial({ color: 0xffffff }), // bottom
+  ]);
+
   cabin.position.x = -6;
   cabin.position.z = 25.5;
   car.add(cabin);
